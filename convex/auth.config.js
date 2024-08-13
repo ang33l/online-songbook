087dcd886f8f55fa7e5aadb55b97a721dc7eb22b
ang33l <aleksandermarek60@gmail.com>
2024-08-13 13:47:27 +0200

    env change

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://improved-mastodon-70.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ],
